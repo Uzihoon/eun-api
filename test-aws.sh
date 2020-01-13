308a2034ce7340ed7dee46ad34054c1b2caa06e2
jiwoohong <uzihoon.dev@gmail.com>
2020-01-13 12:28:39 +0900

    [EUN] api setup

--- test-aws.sh
+++ test-aws.sh
@@ -1,12 +1,12 @@
 npx aws-api-gateway-cli-test \
---username='admin@example.com' \
---password='Passw0rd!' \
---user-pool-id='YOUR_COGNITO_USER_POOL_ID' \
---app-client-id='YOUR_COGNITO_APP_CLIENT_ID' \
---cognito-region='YOUR_COGNITO_REGION' \
---identity-pool-id='YOUR_IDENTITY_POOL_ID' \
---invoke-url='YOUR_API_GATEWAY_URL' \
---api-gateway-region='YOUR_API_GATEWAY_REGION' \
---path-template='/notes' \
+--username='username' \
+--password='password' \
+--user-pool-id='pool-id' \
+--app-client-id='client-id' \
+--cognito-region='region' \
+--identity-pool-id='identity-pool-id' \
+--invoke-url='invoke-url' \
+--api-gateway-region='gateway-region' \
+--path-template='path' \
 --method='POST' \
---body='{"content":"hello world","attachment":"hello.jpg"}'
+--body='{"content":"hello world"}'
